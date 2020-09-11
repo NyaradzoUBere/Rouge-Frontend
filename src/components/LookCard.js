88cd9f532e0de72bc5894e82557af4be354f4f57
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-10 21:33:26 -0600

    allow card in to have 'remove' button

---
+++ src/components/LookCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Modal from '../components/Modal'
+
+export default class Card extends React.Component {
+    state = {
+        modal: false
+    }
+    
+    handleClick = (event) => {
+        const newModal = !this.state.modal
+        this.setState({ modal: newModal })
+    }
+
+    handleLooksClick = () => {
+        this.props.clickLooks(this.props.product)
+        // console.log(this.props.product)
+    }
+
+    render() {
+        return(
+            <>
+                <li className = "product-card">
+                    <img className = "product-image" src = {this.props.product.image_link} onClick = {this.handleClick}/>
+                    <h2>{this.props.product.name}</h2>
+                    <button onClick = {this.handleLooksClick}>Remove From Look</button>
+                </li>
+                {this.state.modal ? <Modal product={this.props.product} handleClick={this.handleClick} image = {this.props.image} name = {this.props.name} showPrice = {this.props.showPrice} brand = {this.props.brand}/> : null}
+            </>
+        )
+    }
+
+}
