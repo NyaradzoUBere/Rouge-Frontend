c9604a35eef380bcb5e1a799a5cbf618b79f6aeb
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-11 12:02:02 -0600

    fix subheader typo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner() {
     return (
         <div className = "banner">
             <h1 className= "banner-title">Rouge</h1>
-            <h1 className = "sub-header">Create Your Look! Daytime or Nightime?</h1>
+            <h1 className = "sub-header">Create Your Look! Daytime or Night Time?</h1>
         </div>
     )
 }
