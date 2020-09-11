8793c6e6c96be6de002b7f355dfa2496d23d2812
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-10 18:19:28 -0600

    add color to looks div

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,8 +6,8 @@ export default function Modal(props) {
         <div className="product-details">
             <h2 className = "product-brand">{props.product.brand}</h2>
             <h2 className = "product-brand">{props.category}</h2>
-            <img src={props.product.image_link} alt={props.name} id= "modal-image" />
-            <h2 className = "product-name">{props.name}</h2>
+            <img src={props.product.image_link} alt={props.product.name} id= "modal-image" />
+            <h2 className = "product-name">{props.product.name}</h2>
             <p className = "product-description">{props.product.description}</p>
             <p className = "price">${props.product.price}</p>
         </div>
