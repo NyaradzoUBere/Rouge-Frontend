2f58e85fe36e4212354cefafd4872e4a35854027
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-11 11:56:46 -0600

    simplify subheader text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner() {
     return (
         <div className = "banner">
             <h1 className= "banner-title">Rouge</h1>
-            <h1 className = "sub-header">Create a Look Based on the Target Time of Day of Your Activities</h1>
+            <h1 className = "sub-header">Create Your Look! Daytime or Nightime?</h1>
         </div>
     )
 }
