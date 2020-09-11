67636303545c43c2f66330fd2c47879549163aca
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-11 11:52:44 -0600

    add subheader to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,7 @@ export default function Banner() {
     return (
         <div className = "banner">
             <h1 className= "banner-title">Rouge</h1>
+            <h1 className = "sub-header">Create a Look Based on the Target Time of Day of Your Activities</h1>
         </div>
     )
 }
